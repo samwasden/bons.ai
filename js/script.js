7d3f4db6c0300de543ad1c8c4ed84c91ab53eb2f
Sam Wasden <samwasden@outlook.com>
2021-08-20 13:27:41 -0600

    added shadow rendering, tree color.

--- js/script.js
+++ js/script.js
@@ -13,23 +13,38 @@ const scene = new THREE.Scene();
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
 
+        renderer.shadowMap.enabled = true;
+        renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
         document.body.appendChild( renderer.domElement );
 
         const ambientLight = new THREE.AmbientLight( 0xE8E8E8, 0.7 ); // soft white light
         scene.add( ambientLight );
 
-        const light = new THREE.DirectionalLight( 0xE8E8E8, 1, 100 );
-        light.position.set(0, 1, 0)
+        const light = new THREE.DirectionalLight( 0xE8E8E8, 1 );
+
+        light.position.set(30, 100, 20)
+        light.castShadow = true;
+
+        light.shadow.mapSize.width = 4000; // default
+        light.shadow.mapSize.height = 4000; // default
+        light.shadow.camera.near = 0.5; // default
+        light.shadow.camera.far = 120; // default
+        light.shadow.camera.top = 100;
+        light.shadow.camera.bottom = -100;
+        light.shadow.camera.left = 100;
+        light.shadow.camera.right = -100;
 
         scene.add( light );
 
         // recursive function creates limbs and joints at random
 
-        function limb(base, shade, ymove = base*2, xmove = 0, zmove = 0, rotatez = 0, rotatex = 0, trunk = true) {
+        function limb(base, shade = 0x464333, ymove = base*2, xmove = 0, zmove = 0, rotatez = 0, rotatex = 0, trunk = true) {
 
             const geometry = new THREE.CylinderGeometry((base*.9)/4, base/4, base*4, 10);
             const material = new THREE.MeshLambertMaterial( {color: shade} );
             const branch = new THREE.Mesh( geometry, material );
+            branch.castShadow = true;
 
             // roatate and change horizontal position
 
@@ -74,7 +89,7 @@ const scene = new THREE.Scene();
 
             // check to see if should generate new limb
 
-            if (Math.random() < .8 && base > .3) {
+            if (Math.random() < .9 && base > .3) {
 
                 newz = rotatez
                 newx = rotatex
@@ -82,7 +97,7 @@ const scene = new THREE.Scene();
                 newz += (Math.random() - .5)
                 newx += (Math.random() - .5)
 
-                limb(base*.6, shade, ymove, xmove, zmove, newz*1.2, newx*1.2, false)
+                limb(base*.5, shade, ymove, xmove, zmove, newz*1.2, newx*1.2, false)
 
                 if (Math.random() < .5 && base > .3) {
 
@@ -92,7 +107,7 @@ const scene = new THREE.Scene();
                     newz2 += (Math.random() - .5)
                     newx2 += (Math.random() - .5)
 
-                    limb(base*.6, shade, ymove, xmove, zmove, newz2*1.2, newx2*1.2, false)
+                    limb(base*.5, shade, ymove, xmove, zmove, newz2*1.2, newx2*1.2, false)
 
                     if (Math.random() < .2 && base > .3) {
 
@@ -102,7 +117,7 @@ const scene = new THREE.Scene();
                         newz3 += (Math.random() - .5)
                         newx3 += (Math.random() - .5)
 
-                        limb(base*.6, shade, ymove, xmove, zmove, newz3*1.2, newx3*1.2, false)
+                        limb(base*.5, shade, ymove, xmove, zmove, newz3*1.2, newx3*1.2, false)
                     }
                 }
             }
@@ -127,18 +142,32 @@ const scene = new THREE.Scene();
 
         //  create a new instance of my tree
 
+        let groundGeometry = new THREE.BoxGeometry(100, 100, 10)
+        let groundMaterial = new THREE.MeshLambertMaterial( {color: 0xffffff} );
+        const ground = new THREE.Mesh( groundGeometry, groundMaterial );
+        ground.rotation.x = -1.5708
+        ground.receiveShadow = true;
+
+        scene.add( ground )
+
+
+
         let treeGroup = new THREE.Group()
-        let tree = limb(2, 0xE8E8E8)
+        let tree = limb(2)
+
+        treeGroup.castShadow = true;
+        treeGroup.receiveShadow = true;
+
         scene.add(treeGroup)
 
         //  Animate Scene
 
-        const animate = function () {
-            requestAnimationFrame( animate );
+        // const animate = function () {
+        //     requestAnimationFrame( animate );
             
-            treeGroup.rotation.y += 0.005
+        //     treeGroup.rotation.y += 0.005
 
             renderer.render( scene, camera );
-        };
+        // };
 
-        animate();
+        // animate();
